7e6f19fa57d6b0d9607164a7356f991ef7963134
joshrod94 <47401286+joshrod94@users.noreply.github.com>
2026-01-02 18:18:11 -0500

    Initialize Spring Boot Maven project

---
+++ src/main/java/com/joshrod94/userdatanormalizer/UserDataNormalizerApplication.java
@@ -0,0 +1,13 @@
+package com.joshrod94.userdatanormalizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDataNormalizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserDataNormalizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joshrod94/userdatanormalizer/UserDataNormalizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joshrod94.userdatanormalizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDataNormalizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
