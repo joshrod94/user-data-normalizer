b6fc094f41c40a76fddaf7607dd6efef4ad0645f
joshrod94 <47401286+joshrod94@users.noreply.github.com>
2026-01-02 18:58:24 -0500

    Add REST controller and service for user normalization pipeline

---
+++ src/main/java/com/joshrod94/userdatanormalizer/UserNormalizationController.java
@@ -0,0 +1,23 @@
+package com.joshrod94.userdatanormalizer;
+
+import com.joshrod94.userdatanormalizer.service.UserNormalizationService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserNormalizationController {
+
+    private final UserNormalizationService userNormalizationService;
+
+    public UserNormalizationController(UserNormalizationService userNormalizationService) {
+        this.userNormalizationService = userNormalizationService;
+    }
+
+    @PostMapping("/normalize")
+    public String normalizeUsers() {
+        userNormalizationService.normalizeUsers();
+        return "User normalization triggered";
+    }
+}
---
+++ src/main/java/com/joshrod94/userdatanormalizer/service/UserNormalizationService.java
@@ -0,0 +1,11 @@
+package com.joshrod94.userdatanormalizer.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserNormalizationService {
+
+    public void normalizeUsers() {
+        // Pipeline logic will go here later
+    }
+}
